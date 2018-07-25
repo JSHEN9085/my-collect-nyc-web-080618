a06184a50864493d07261bd9057b607ffb6e1f48
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 16:01:08 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,8 @@
 def my_collect(array)
-    counter = 0
-    array.collect do |x|
-      counter += 1
+  new_array = []
+  counter = 0
+  array.collect do |x|
+    counter += 1
   end
   return array 
 end 
