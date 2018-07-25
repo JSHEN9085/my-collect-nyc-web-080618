321f387f82b85bf87823ec2dd89d32f2fd00618e
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 16:04:22 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   new_array = []
   counter = 0
   array.collect do |x|
-    new_array.push(array[i])
+    new_array.push(array[i].join(" ").first)
     counter += 1
   end
   return new_array 
